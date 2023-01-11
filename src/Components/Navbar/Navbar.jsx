a650e42c6eaf1c17a6bf2865cc35991839bbd1f2
Israel <iaboluwatife@gmail.com>
2023-01-11 17:17:01 -0500

    Created a Navbar component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <img src="" alt=""  />
+    </div>
+  )
+}
+
+export default Navbar
--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import './Landingpage.scss';
+import logo from '../logo.svg';
 const Landingpage = () => {
   return (
     <div>
-        
+        <img src={logo} alt="" srcset="" />
+    <h1>kasjksdjksdjksdjksdjksd</h1>
     </div>
   )
 }
