a7dfff75503f1e3f0fd766dd42d5e7503499dfe8
Israel <iaboluwatife@gmail.com>
2023-01-17 05:59:55 -0500

    Installed react file uploader dependency

--- src/Components/Propertyform/Propertyform.jsx
+++ src/Components/Propertyform/Propertyform.jsx
@@ -5,20 +5,68 @@ const Propertyform = () => {
     <div className='pForm-container'>
         <h1>Add A New Property</h1>
         <form action="" method="post">
-            <section>
+            <section className='form-flex'>
                 <div>
-                    <label htmlFor="name">Name *</label>
+                    <label htmlFor="name">Name<span>*</span></label>
                     <input type="text" name="name" placeholder='Enter Name' />
                 </div>
                 <div>
-                    <label htmlFor="address">Address *</label>
+                    <label htmlFor="address">Address<span>*</span></label>
                     <input type="text" name="address" placeholder='Enter Address'/>
                 </div>
                 <div>
-                    <label htmlFor="unit">Unit Number *</label>
+                    <label htmlFor="unit">Unit Number<span>*</span></label>
                     <input type="number" name="unit" placeholder='Enter Unit'/>
                 </div>
+                <div>
+                    <label htmlFor="city">City<span>*</span></label>
+                    <select name="city" placeholder='Enter City'>
+                        <option defaultValue="city"></option>
+                        <option value="ikeja">Ikeja</option>
+                        <option value="abuja">Abuja</option>
+                        <option value="ph">Port Harcourt</option>
+                        <option value="ibadan">Ibadan</option>
+                        <option value="onisha">Onitsha</option>
+                        <option value="akure">Akure</option>
+                        <option value="owerri">Owerri</option>
+                        <option value="onitsha">Onitsha</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="state">State<span>*</span></label>
+                    <select name="state" placeholder='Enter State'>
+                        <option defaultValue="state"></option>
+                        <option value="lagos">Lagos State</option>
+                        <option value="Rivers">Rivers State</option>
+                        <option value="ogun">Ogun State</option>
+                        <option value="oyo">Oyo</option>
+                        <option value="imo">Imo</option>
+                        <option value="enugu">Enugu</option>
+                        <option value="kaduna">Kaduna</option>
+                        <option value="onitsha">Onitsha</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="price">Price<span>*</span></label>
+                    <input type="text" name="price" placeholder='Enter Price'/>
+                </div>
+                <div>
+                    <label htmlFor="room">Room Type<span>*</span></label>
+                    <select name="room" placeholder='Room Type'>
+                    <option defaultValue="roomtype"></option>
+                        <option value="l2bedroomflat">2 Bedroom Flat</option>
+                        <option value="3bedroomflat">3 Bedroom Flat</option>
+                        <option value="duplex">Duplex</option>
+                        <option value="suite">Suite</option>
+                    </select>
+                </div>
             </section>
+            <div className='descrpt-div'>
+                <label htmlFor="description">Description<span>*</span></label>
+                <textarea name="description" cols="30" rows="10" placeholder='Enter Description'></textarea>
+            </div>
+
+            <button>Add New Property</button>
         </form>
     </div>
   )
