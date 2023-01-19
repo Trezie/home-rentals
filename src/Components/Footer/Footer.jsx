ff821ca035d44f535dd71099b88fc06710398bb8
Israel <iaboluwatife@gmail.com>
2023-01-18 19:34:17 -0500

    Created a new component for Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+        <hr />
+    </div>
+  )
+}
+
+export default Footer
--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -15,6 +15,7 @@ import plant from '../Assets/plant.svg';
 import eye from '../Assets/eye.svg';
 import shield from '../Assets/shield.svg';
 import caropic from '../Assets/carousel-pic.svg';
+import caroavatar from '../Assets/caro-avatar.svg';
 
 
 
@@ -113,44 +114,69 @@ const Landingpage = () => {
           <Carousel className='carousel' autoPlay='true' infiniteLoop='true' interval='3000' showThumbs={false}>
             <div className='caro-div'>
               <img id='caro-pic' src={caropic} alt="caropic" />
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Etiam interdum nisl et nunc facilisis, a commodo eros mollis. 
-              Nunc vel pellentesque est. Curabitur at odio sit amet libero 
-              vulputate efficitur ac nec justo. Nulla vitae mauris quam. 
-              Nulla quam massa, faucibus id pretium ac, mattis eu velit. 
-              Donec sed risus a lacus fringilla finibus.
+              <p className="caro-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam interdum nisl et nunc facilisis, a commodo eros mollis. 
+                Nunc vel pellentesque est. Curabitur at odio sit amet libero 
+                vulputate efficitur ac nec justo. Nulla vitae mauris quam. 
+                Nulla quam massa, faucibus id pretium ac, mattis eu velit. 
+                Donec sed risus a lacus fringilla finibus. Nulla quam massa, 
+                faucibus id pretium ac, mattis eu velit. Donec sed risus a 
+                lacus fringilla finibus. mattis eu velit. Donec sed risus 
+                a lacus fringilla finibus
               </p>
-              <div>
-                  <img src="" alt="" />
+              <div className="caro-footer">
+                  <img src={caroavatar} alt="avatar" id="caro-avatar" />
                   <div>
-                    <p>Harry Wilson</p>
+                    <p className="caro-name">Harry Wilson</p>
                     <p>Property Owner</p>
                   </div>
               </div>
             </div>
             <div className='caro-div'>
                 <img id='caro-pic' src={caropic} alt="caropic" />
-                <p>
-                Orci varius natoque penatibus et magnis dis parturient montes, 
-                nascetur ridiculus mus. Vestibulum imperdiet tortor eu odio 
-                rutrum venenatis. Curabitur sodales ac felis sollicitudin egestas. 
-                Ut lacinia efficitur dolor, at consectetur urna viverra at. 
-                Sed nisi eros, auctor at volutpat sit amet, volutpat et nulla. 
-                Pellentesque volutpat odio non mi iaculis, id malesuada mauris aliquam. 
-                Donec id vulputate velit, nec tincidunt sapien. Integer auctor 
-                vitae urna sit amet rhoncus.
+                <p className="caro-content">
+                  Orci varius natoque penatibus et magnis dis parturient montes, 
+                  nascetur ridiculus mus. Vestibulum imperdiet tortor eu odio 
+                  rutrum venenatis. Curabitur sodales ac felis sollicitudin egestas. 
+                  Ut lacinia efficitur dolor, at consectetur urna viverra at. 
+                  Sed nisi eros, auctor at volutpat sit amet, volutpat et nulla. 
+                  Pellentesque volutpat odio non mi iaculis, id malesuada mauris aliquam. 
+                  Donec id vulputate velit, nec tincidunt sapien. Integer auctor 
+                  vitae urna sit amet rhoncus.
                 </p>
+                <div className="caro-footer">
+                  <img src={caroavatar} alt="avatar" id="caro-avatar" />
+                  <div>
+                    <p className="caro-name">Harry Wilson</p>
+                    <p>Property Owner</p>
+                  </div>
+              </div>
             </div>
             <div className='caro-div'>
               <img id='caro-pic' src={caropic} alt="caropic" />
-              <p>
-              Etiam sit amet lacus nisl. Integer fermentum neque urna, non ornare urna condimentum sed. Aliquam justo ligula, efficitur nec est rutrum, tincidunt malesuada nibh. Nunc est tellus, facilisis at dui vel, tristique placerat neque. Duis tempor velit ante, et porttitor arcu viverra nec. Mauris vel metus augue. Cras dictum sem vitae pretium feugiat. Nunc eleifend faucibus mi, et pellentesque urna scelerisque vitae. Vivamus finibus pharetra gravida. Praesent interdum justo erat, ac cursus mi facilisis id. 
+              <p className="caro-content">
+                Etiam sit amet lacus nisl. Integer fermentum neque urna, 
+                non ornare urna condimentum sed. Aliquam justo ligula, 
+                efficitur nec est rutrum, tincidunt malesuada nibh. Nunc 
+                est tellus, facilisis at dui vel, tristique placerat neque. 
+                Duis tempor velit ante, et porttitor arcu viverra nec. 
+                Mauris vel metus augue. Cras dictum sem vitae pretium feugiat. 
+                Nunc eleifend faucibus mi, et pellentesque urna scelerisque 
+                vitae. Vivamus finibus pharetra gravida. Praesent interdum 
+                justo erat, ac cursus mi facilisis id. 
               </p>
+              <div className="caro-footer">
+                  <img src={caroavatar} alt="avatar" id="caro-avatar" />
+                  <div>
+                    <p className="caro-name">Harry Wilson</p>
+                    <p>Property Owner</p>
+                  </div>
+              </div>
             </div>
           </Carousel>
-          <div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/5Peo-ivmupE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className="youtube">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/5Peo-ivmupE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
        </section>
     </div>
